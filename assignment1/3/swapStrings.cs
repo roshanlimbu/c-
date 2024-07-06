32278451fdc83c08e3828eb99262afac074f5322
roshanlimbu <birenlimbu415@gmail.com>
2024-07-06 20:29:32 +0545

    swap strings

---
+++ assignment1/3/swapStrings.cs
@@ -0,0 +1,18 @@
+using System;
+
+class swapString
+{
+    static void Main(String[]args)
+    {
+        string str1 = "Roshan";
+        string str2 = "Limbu";
+        string temp; 
+        Console.WriteLine($"Before swapping: str1 = {str1}, str2 = {str2}");
+
+        temp = str1;
+        str1 = str2;
+        str2 = temp;
+
+        Console.WriteLine($"After swapping: str1 = {str1}, str2 = {str2}");
+    }
+}
